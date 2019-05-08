13f8a0ff092b4422a48b78b093fc7f3f020e0661
aereeeee <dofl5576@gmail.com>
2019-05-08 15:51:35 +0900

    Add routes

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,37 @@
 var express=require('express');
 var router=express.Router();
-// var cam =require('../models/cam');
+var Cam =require('../models/cam');
 
 router.get('/api/cams', function(req,res){
-    res.send('result')
+    Cam.find(function(err,cams){
+        if(err) return res.status(500).send({error:'database failure'});
+        res.json(cams);
+    })
 });
 router.get('/api/cams/:accId', function(req,res){
-    res.end();
+   Cam.find({accId:req.params.accId},function(err, cams){
+       if(err) return res.status(500),json({error:err});
+       if(!cams) return res.status(404).json({error:'선택한 유형에 대한 데이터가 없습니다'});
+       res.json(cams);
+   })
 });
 router.post('/api/cams', function(req,res){
-    res.end();
+    var cam= new Cam();
+    cam.accId=req.body.accId;
+    cam.answer1=req.body.answer1;
+    cam.answer2=req.body.answer2;
+    cam.answer3=req.body.answer3;
+    cam.sex=req.body.sex;
+    cam.age=req.body.age;
+    
+    cam.save(function(err){
+        if(err){
+            console.error(err);
+            res.json({result:0});
+            return;
+        }
+        res.json({result:1});
+    });
 });
 
-// router.post('/')
 module.exports=router;
